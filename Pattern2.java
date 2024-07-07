78924f6077d992e70aca9c3b599cb66b03a93568
Pavneet Singh Bhatia <97412282+PavneetSinghh@users.noreply.github.com>
2024-07-07 18:38:49 +0530

    Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -4,6 +4,11 @@
 //****
 //*****
 
+//rule 1-for outer loop check rows
+//rule 2-for inner loop,check cols and connect to rows
+//rule 3-print pattern inside inner loop
+//rule 4-check symmetry(optional)
+
 class Pattern2
 {
     public static void main(String args[])
