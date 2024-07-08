2943c306992b0920b1001465348e2409a1b692f9
Pavneet Singh Bhatia <97412282+PavneetSinghh@users.noreply.github.com>
2024-07-08 14:19:45 +0530

    Add files via upload

---
+++ Pattern5.java
@@ -0,0 +1,20 @@
+//*****
+//****
+//***
+//**
+//*
+
+class Pattern5
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=1;j<5-i+1;j++)
+            {
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+    }
+}
